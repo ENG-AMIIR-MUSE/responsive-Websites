7f74716443153c2f2d95106e73db7db8c1fd8b6b
AmiirCloud <123984574+AmiirCloud@users.noreply.github.com>
2023-01-30 10:18:35 -0800

    Add files via upload

---
+++ main - Copy.js
@@ -0,0 +1,5 @@
+let burger  =  document.querySelector(".burger")
+let nav =  document.querySelector(".navigation-bar")
+burger.addEventListener("click",function(){
+    nav.classList.toggle("active")
+})
